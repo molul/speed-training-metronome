6582b810b63d1ba1faf3cd8e20dfecbf6596dfcd
molul <luismorcillo@gmail.com>
2026-02-20 00:21:03 +0100

    Update SettingsDialog.vue

--- src/components/SettingsDialog.vue
+++ src/components/SettingsDialog.vue
@@ -76,7 +76,7 @@ type BpmKey = keyof typeof labels
       <CheckBoxControl label="Stop at end" v-model="store.config.stopAtEnd" />
     </div>
 
-    <hr class="border-zinc-600" />
+    <hr class="border-zinc-400" />
 
     <div class="flex flex-col gap-4 w-full">
       <MyButton
